e978671fd00a9fc7e727a0c4cc43719b7eb105e7
Dominic Monroe <iam@dominic.io>
2019-12-30 13:24:37 +0000

    Reformat code
    I've learned how to add newlines in zig fmt

--- pulse-vol.zig
+++ pulse-vol.zig
@@ -12,7 +12,12 @@ fn volume_as_percent(cvol: pulse.pa_cvolume) u32 {
     return (pulse.pa_cvolume_max(&cvol) * 100 + (PA_VOLUME_NORM / 2)) / PA_VOLUME_NORM;
 }
 
-extern fn get_sink_info_callback(c: ?*pulse.struct_pa_context, optional_sink: ?*const pulse.struct_pa_sink_info, islast: c_int, optional_userdata: ?*c_void) void {
+extern fn get_sink_info_callback(
+    c: ?*pulse.struct_pa_context,
+    optional_sink: ?*const pulse.struct_pa_sink_info,
+    islast: c_int,
+    optional_userdata: ?*c_void,
+) void {
     if (optional_sink) |sink| {
         if (optional_userdata) |user_data| {
             const default_sink = toSliceConst(u8, @ptrCast([*:0]const u8, user_data));
@@ -24,36 +29,57 @@ extern fn get_sink_info_callback(c: ?*pulse.struct_pa_context, optional_sink: ?*
     }
 }
 
-extern fn get_server_info_cb(c: ?*pulse.struct_pa_context, optional_server_info: ?*const pulse.struct_pa_server_info, userdata: ?*c_void) void {
+extern fn get_server_info_cb(
+    c: ?*pulse.struct_pa_context,
+    optional_server_info: ?*const pulse.struct_pa_server_info,
+    userdata: ?*c_void,
+) void {
     if (optional_server_info) |server_info| {
         var default_sink_name = server_info.default_sink_name;
         // This intToPtr/ptrToInt is here to workaround the fact that
         // default_sink_name is a const, but we're casting it to a non-const by
         // using it in a *c_void location.  This is okay because the
         // get_sink_info_callback casts it back to a const later.
-        const o = pulse.pa_context_get_sink_info_list(c, get_sink_info_callback, @intToPtr(*c_void, @ptrToInt(default_sink_name)));
+        const o = pulse.pa_context_get_sink_info_list(
+            c,
+            get_sink_info_callback,
+            @intToPtr(*c_void, @ptrToInt(default_sink_name)),
+        );
         pulse.pa_operation_unref(o);
     }
 }
 
-extern fn context_subscribe_callback(c: ?*pulse.struct_pa_context, t: pulse.pa_subscription_event_type_t, idx: u32, userdata: ?*c_void) void {
+extern fn context_subscribe_callback(
+    c: ?*pulse.struct_pa_context,
+    t: pulse.pa_subscription_event_type_t,
+    idx: u32,
+    userdata: ?*c_void,
+) void {
     const o = pulse.pa_context_get_server_info(c, get_server_info_cb, null);
     pulse.pa_operation_unref(o);
 }
 
-extern fn context_state_callback(c: ?*pulse.pa_context, userdata: ?*c_void) void {
+extern fn context_state_callback(
+    c: ?*pulse.pa_context,
+    userdata: ?*c_void,
+) void {
     if (pulse.pa_context_get_state(c) == pulse.PA_CONTEXT_READY) {
         pulse.pa_context_set_subscribe_callback(c, context_subscribe_callback, null);
 
-        const o = pulse.pa_context_subscribe(c, @intToEnum(pulse.enum_pa_subscription_mask, @enumToInt(pulse.PA_SUBSCRIPTION_MASK_SINK) |
-            @enumToInt(pulse.PA_SUBSCRIPTION_MASK_SOURCE) |
-            @enumToInt(pulse.PA_SUBSCRIPTION_MASK_SINK_INPUT) |
-            @enumToInt(pulse.PA_SUBSCRIPTION_MASK_SOURCE_OUTPUT) |
-            @enumToInt(pulse.PA_SUBSCRIPTION_MASK_MODULE) |
-            @enumToInt(pulse.PA_SUBSCRIPTION_MASK_CLIENT) |
-            @enumToInt(pulse.PA_SUBSCRIPTION_MASK_SAMPLE_CACHE) |
-            @enumToInt(pulse.PA_SUBSCRIPTION_MASK_SERVER) |
-            @enumToInt(pulse.PA_SUBSCRIPTION_MASK_CARD)), null, null);
+        const o = pulse.pa_context_subscribe(
+            c,
+            @intToEnum(pulse.pa_subscription_mask, @enumToInt(pulse.PA_SUBSCRIPTION_MASK_SINK) |
+                @enumToInt(pulse.PA_SUBSCRIPTION_MASK_SOURCE) |
+                @enumToInt(pulse.PA_SUBSCRIPTION_MASK_SINK_INPUT) |
+                @enumToInt(pulse.PA_SUBSCRIPTION_MASK_SOURCE_OUTPUT) |
+                @enumToInt(pulse.PA_SUBSCRIPTION_MASK_MODULE) |
+                @enumToInt(pulse.PA_SUBSCRIPTION_MASK_CLIENT) |
+                @enumToInt(pulse.PA_SUBSCRIPTION_MASK_SAMPLE_CACHE) |
+                @enumToInt(pulse.PA_SUBSCRIPTION_MASK_SERVER) |
+                @enumToInt(pulse.PA_SUBSCRIPTION_MASK_CARD)),
+            null,
+            null,
+        );
 
         pulse.pa_operation_unref(o);
 
